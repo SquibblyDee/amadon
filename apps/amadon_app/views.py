962a5d68d44e1badae613d469ddcd90db039e203
unknown <steelst0@gmail.com>
2018-07-16 14:54:45 -0700

    all desired functionality complete

--- apps/amadon_app/views.py
+++ apps/amadon_app/views.py
@@ -5,14 +5,27 @@ from django.contrib import messages
 from .models import Store
 
 def index(request):
+    if 'count' or 'product_id' or 'total_spent' or 'total' not in request.session:
+        request.session['count'] = 0
+        request.session['product_id'] = 0
+        request.session['total_spent'] = 0.00
+        request.session['total'] = 0
     query = Store.objects.values('id', 'name', 'price')
     return render(request,'amadon_app/index.html', {'query' : query})
 
 def process(request, methods=['POST']):
-    print(request.POST['product_id'])
-    print(request.POST['quantity'])
-    # print("PRODUCT ID ", product_id)
-    # print("QUANTITY ", quantity)
+    quantity = request.POST['quantity']
+    query = Store.objects.filter(id=request.POST['product_id']).values('price')
+    print("QUERY: ", query)
+    for row in query:
+        for key in row:
+            price = float(row[key])
+            request.session['total'] = price * float(quantity)
+            request.session['count']+=int(quantity)
+            request.session['total_spent']+=request.session['total']
+            print("TOTAL: ", request.session['total'])
+            print("COUNT: ", request.session['count'])
+            print("TOTALSPENT ", request.session['total_spent'])
     return redirect('/amadon/checkout')
 
 def checkout(request):
